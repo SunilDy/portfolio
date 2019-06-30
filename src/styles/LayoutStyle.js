f541082e6f07a9d800e64b28850c6ccf7173eeaa
SunilDy <sunilsahudy@hotmail.com>
2019-06-30 15:54:27 +0530

    setting up blog

--- src/styles/LayoutStyle.js
+++ src/styles/LayoutStyle.js
@@ -1,19 +1,19 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: 0 auto;
-    padding: 20px 0 20px 0;
-    max-width: 70vw;
-    min-height: 100vh;
-    justify-content: space-evenly;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 20px 0 20px 0;
+  max-width: 70vw;
+  min-height: 100vh;
+  justify-content: space-evenly;
+  align-items: center;
 
-        > Header {
-            color: #333;
-        }
-        > Footer {
-            color: blue;
-        }
+  > header {
+    color: #333;
+  }
+  > footer {
+    color: blue;
+  }
 `
